04856c3ef9a034fc701244bb33d9094f51155222
Konrad <konrad.narewski@humanoitgroup.com>
2014-08-18 12:33:03 +0200

    Reproducing the problem

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,11 @@
 class Post < ActiveRecord::Base
+  acts_as_nested_set
+  after_save :print_changes
+
+  private
+
+  def print_changes
+    puts "Changes:"
+    p changes
+  end
 end
--- db/migrate/20140818101845_create_posts.rb
+++ db/migrate/20140818101845_create_posts.rb
@@ -1,7 +1,11 @@
 class CreatePosts < ActiveRecord::Migration
   def change
     create_table :posts do |t|
-
+      t.string :text
+      t.integer :parent_id
+      t.integer :lft
+      t.integer :rgt
+      t.integer :depth # this is optional.
       t.timestamps
     end
   end
